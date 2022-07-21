ae0020e4f89653d7d5c52ecdafba1b3d9d826c76
lihugang <lihugang@outlook.com>
2022-07-21 22:15:40 +0800

    client: tool class common.js add logger and StringBuilder

--- client-side/common.js
+++ client-side/common.js
@@ -1,30 +1,18 @@
-/**
- * @Author          : lihugang
- * @Date            : 2022-05-18 17:02:29
- * @LastEditTime    : 2022-05-31 11:02:55
- * @LastEditors     : lihugang
- * @Description     : 
- * @FilePath        : \client-base\common.js
- * @Copyright (c) lihugang
- * @长风破浪会有时 直挂云帆济沧海
- * @There will be times when the wind and waves break, and the sails will be hung straight to the sea.
- * @ * * * 
- * @是非成败转头空 青山依旧在 几度夕阳红
- * @Whether it's right or wrong, success or failure, it's all empty now, and it's all gone with the passage of time. The green hills of the year still exist, and the sun still rises and sets.
- */
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
-async function makeRequest(obj, env) {
-    obj = replaceObjects(obj, env);
+const chalk = require('chalk');
+
+async function makeRequest(obj, env) { //make request with config.yaml
+    obj = replaceObjects(obj, env); //replace variables
     obj.request.headers = obj.request.headers || {};
-    obj.request.headers["X-PLATFORM"] = process.platform;
-    var response = await fetch(obj.request.url, {
+    obj.request.headers['X-PLATFORM'] = process.platform; //Platform set
+    var response = await fetch(obj.request.url, { //browser like fetch
         headers: obj.request.headers || { 'x-platform': process.platform },
         method: obj.request.method || 'GET',
         body: obj.request.body ? ((obj.request.body.encode == 'JSON') ? JSON.stringify(obj.request.body.data) : obj.request.body.data.toString()) : (void 0)
     });
-    if (response.status == obj.response.success || obj.response.success == "all") {
+    if (response.status == obj.response.success || obj.response.success == 'all') {
         var dataObj = {};
         if (obj.response.data == 'all') dataObj = response.response;
         else {
@@ -37,14 +25,13 @@ async function makeRequest(obj, env) {
         return ['success', dataObj, response.status, response.headers];
     } else {
         return ['failure', response.response, response.status, response.headers];
-    };
+    }; //get objects
 };
 
 function fetch(reqPath, options = {}) {
-    console.log(`Fetch`, reqPath, options);
-    var url = new URL(reqPath);
+    var url = new URL(reqPath); //parse url
     return new Promise(function (resolve, reject) {
-        const reqClass = (url.protocol == 'https:') ? https : http;
+        const reqClass = (url.protocol == 'https:') ? https : http; //choose protocol
         const req = reqClass.request({
             host: url.hostname,
             port: url.port,
@@ -52,28 +39,19 @@ function fetch(reqPath, options = {}) {
             method: options.method || 'GET',
             headers: options.headers || {}
         }, res => {
-            //var data = new Buffer();
-            var data = "";
-            res.on("data", chunk => {
-                // for (var i = 0; i < chunk.length; i++) {
-                //     data.push(chunk[i]);
-                // };
+            var data = '';
+            res.on('data', chunk => {
                 data += chunk;
             });
-            res.on("end", () => {
-                console.log({
-                    status: res.statusCode,
-                    headers: res.headers,
-                    response: data
-                });
+            res.on('end', () => {
                 resolve({
                     status: res.statusCode,
                     headers: res.headers,
                     response: data
                 });
-            })
+            });
         });
-        if (options.body) req.write(options.body);
+        if (options.body) req.write(options.body, 'binary');
         req.end();
     });
 
@@ -81,9 +59,9 @@ function fetch(reqPath, options = {}) {
 
 function replaceObjects(obj, env) {
     for (var key in obj) {
-        if (typeof obj[key] == "object") {
+        if (typeof obj[key] == 'object') {
             obj[key] = replaceObjects(obj[key], env);
-        } else if (typeof obj[key] == "string") {
+        } else if (typeof obj[key] == 'string') {
             obj[key] = replaceString(obj[key], env);
         };
     };
@@ -91,7 +69,7 @@ function replaceObjects(obj, env) {
 };
 
 function replaceString(s, globalEnvironment = {}) {
-    if (typeof s != "string") return s;
+    if (typeof s != 'string') return s;
     var match_arrays = s.match(/\${.*?}/gim);
     if (match_arrays == null) return s;
     for (var i = 0; i < match_arrays.length; i++) {
@@ -100,27 +78,7 @@ function replaceString(s, globalEnvironment = {}) {
     return s;
 };
 
-function convertResponse(data) {
-    if (data[0] == 'success') return JSON.stringify({
-        status: "success",
-        data: data[1]
-    });
-    else return JSON.stringify({
-        status: data[0],
-        data: {
-            source_return: data[1]
-        },
-        code: data[2]
-    })
-};
-
-const _iRequire_record = [];
-
-function iRequire(dest) {
-    if (_iRequire_record[dest]) return _iRequire_record[dest];
-    else _iRequire_record[dest] = require(dest);
-    return iRequire(dest);
-};
+//get sql from disk
 _sqlCache = new Map();
 function getSQL(key/*,varibles = {}*/) {
     if (_sqlCache.has(key)) var sql = _sqlCache.get(key);
@@ -135,19 +93,95 @@ function getSQL(key/*,varibles = {}*/) {
 
 };
 
-function sqlReplace(s, varibles) {
-    var match_arrays = s.match(/{.*?}/gim);
-    if (match_arrays == null) return s;
-    for (var i = 0; i < match_arrays.length; i++) {
-        s = s.replace(match_arrays[i], varibles[match_arrays[i].substring(1, match_arrays[i].length - 1)] || match_arrays[i]);
-    };
-    return s;
-};
-
 function sqlInjectionTest(sql) {
     const matchRegExp = /create\s|drop\s|table\s|primary\s|key\s|insert\s|into\s|values\s|select\s|and\s|between\s|exists\s|in\s|like\s|glob\s|not\s|or\s|is\s|null\s|not\s|unique\s|where\s|update\s|limit\s|order\s|group\s|having\s|distinct\s|pragma\s|\s by|and'|or'|and\d|or\d|\sfrom|from\s|\swhere/gim;
     return !!sql.match(matchRegExp);
-}
+};
+
+function StringBuilder(str = '') {
+    this.array = new Array(str.length);
+    for (var i = 0, len = str.length; i < len; ++i) {
+        this.array[i] = str[i];
+    };
+};
+StringBuilder.prototype.append = function (...str) {
+    for (var i = 0, len = str.length; i < len; ++i) {
+        if (str[i] instanceof StringBuilder) {
+            for (var j = 0, jlen = str[i].length; j < jlen; ++j) this.array.push(str[i].array[j]);
+        } else if (typeof str[i] === 'string') {
+            for (var j = 0, jlen = str[i].length; j < jlen; ++j) this.array.push(str[i][j]);
+        } else this.array.push(str[i].toString());
+    };
+};
+StringBuilder.prototype.toString = function () {
+    return this.array.join('');
+};
+StringBuilder.prototype.clear = function () {
+    this.array.length = 0;
+};
+
+function logger(env = 'default') { //env: the environment of logger
+    this.env = env;
+    this.level = logger.INFO;
+}; //logger system
+logger.ALL = 0;
+logger.TRACE = 1;
+logger.DEBUG = 2;
+logger.INFO = 3;
+logger.WARN = 4;
+logger.ERROR = 5;
+logger.FATAL = 6;
+logger.OFF = 7;
+logger.level_color = [
+    0,
+    'gray', //trace
+    'green', //debug
+    'white', //info
+    'yellow', //warn
+    'orange', //error
+    'red', //fatal 
+];
+logger.level_name = [
+    'ALL',
+    'TRACE',
+    'DEBUG',
+    'INFO',
+    'WARN',
+    'ERROR',
+    'FATAL',
+    'OFF',
+];
+logger.prototype.filter = function (level = logger.INFO) {
+    this.level = level;
+};// set logger level
+logger.prototype.print = function (level, msg) {
+    if (level < this.level) return -1; //not print
+    const format_string = new StringBuilder();
+    format_string.append(new Date().toISOString(), ' ', this.env, ' ');
+    return this.print_to_console(format_string, level, msg); //build basic output message
+};
+logger.prototype.print_to_console = function (format_string, level, msg) {
+    format_string.append(chalk[logger.level_color[level]](logger.level_name[level]), ' ');
+    for (var i = 0, len = msg.length; i < len; ++i) {
+        format_string.append(
+            (msg[i] instanceof Error) ? (
+                new StringBuilder().append(msg[i].message, '\n', msg[i].stack).toString()
+            ) : (
+                (typeof msg[i] === 'object') ? (
+                    JSON.stringify(msg[i])
+                ) : msg[i].toString()
+            )
+            , ' ');
+    };
+    console.log(format_string.toString());
+    return 0;
+};
+logger.prototype.trace = function (...msg) { return this.print(logger.TRACE, msg) };
+logger.prototype.debug = function (...msg) { return this.print(logger.DEBUG, msg) };
+logger.prototype.info = function (...msg) { return this.print(logger.INFO, msg) };
+logger.prototype.warn = function (...msg) { return this.print(logger.WARN, msg) };
+logger.prototype.error = function (...msg) { return this.print(logger.ERROR, msg) };
+logger.prototype.fatal = function (...msg) { return this.print(logger.FATAL, msg) };
 
 module.exports = {
     makeRequest,
@@ -157,5 +191,7 @@ module.exports = {
     convertResponse,
     iRequire,
     getSQL,
-    sqlInjectionTest
+    sqlInjectionTest,
+    StringBuilder,
+    logger
 };
