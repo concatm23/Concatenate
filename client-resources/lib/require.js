4c24a57cb87829b721437c1d9270a21322c95987
lihugang <lihugang@outlook.com>
2022-07-30 19:10:28 +0800

    Client: Resources: require.js add cache, avoid requesting repeatly

--- client-resources/lib/require.js
+++ client-resources/lib/require.js
@@ -1,7 +1,7 @@
 /**
  * @Author          : lihugang
  * @Date            : 2022-07-22 13:54:21
- * @LastEditTime    : 2022-07-25 12:54:21
+ * @LastEditTime    : 2022-07-30 17:45:02
  * @LastEditors     : lihugang
  * @Description     : 
  * @FilePath        : c:\Users\heche\AppData\Roaming\concatenate.pz6w7nkeote\resources\lib\require.js
@@ -12,7 +12,9 @@
  * @是非成败转头空 青山依旧在 几度夕阳红
  * @Whether it's right or wrong, success or failure, it's all empty now, and it's all gone with the passage of time. The green hills of the year still exist, and the sun still rises and sets.
  */
+const _requireCache = new Map();
 const fRequire = function _require(module) {
+    if (_requireCache.has(module)) return _requireCache.get(module);
     if (window.XMLHttpRequest) var xhr = new XMLHttpRequest();
     else var xhr = new ActiveXObject('Microsoft.XMLHTTP');
     xhr.open('GET', module, false); //get source code sync
@@ -35,10 +37,12 @@ const fRequire = function _require(module) {
     evalFunc(); //run and parse
     let ret_module = window.module.exports;
     window.module.exports = null; //clear
+    _requireCache.set(module, ret_module);
     return ret_module;
 };
 const fRequireAsync = async function _requireAsync(module) {
     return new Promise(async (resolve, reject) => {
+        if (_requireCache.has(module)) return resolve(_requireCache.get(module));
         const response = await fetch(module);
         if (!response.ok) throw new Error('Cannot find module ' + module);
         var code = await(response).text();
@@ -53,6 +57,7 @@ const fRequireAsync = async function _requireAsync(module) {
         evalFunc(); //run and parse
         let ret_module = window.module.exports;
         window.module.exports = null; //clear
+        _requireCache.set(module, ret_module);
         resolve(ret_module);
 
     });
