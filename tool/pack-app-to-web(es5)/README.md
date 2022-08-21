d18dfa119cfec55d8eeddeeb48d879f59070db65
lihugang <lihugang@outlook.com>
2022-08-21 13:17:15 +0800

    Tools: add tool for packing app side to web side(es6 -> es5 to support some old browsers like IE, Safari, Chrome in Android

---
+++ tool/pack-app-to-web(es5)/README.md
@@ -0,0 +1,16 @@
+
+# Concatenate    
+## Tools  
+### Pack app to web (es5)
+- - -
+How to use:  
+- Install dependencies
+- Put resources directory in `./resource-app`
+- Run `node pack`
+- OK
+- - -
+**Notice:the sdk and FaaS in the app side and the web side is different.**  
+**This tool cannot convert, please recode these.**  
+**.html .css .png .jpeg .woff i18n does not converting.**
+- - - 
+LICENSE: GPL v3
---
+++ tool/pack-app-to-web(es5)/pack.js
@@ -0,0 +1,146 @@
+const fs = require('fs/promises');
+const webpack = require('webpack');
+const path = require('path');
+const webpackPromise = (config) => {
+    return new Promise((resolve, reject) => {
+        webpack(config, (err, result) => {
+            if (err) {
+                console.error(chalk.red(err));
+                reject(err);
+                return;
+            };
+            if (result.hasErrors()) {
+                console.error(chalk.red(result.compilation.errors));
+                reject(err);
+            };
+            resolve(result);
+        });
+    });
+};
+const chalk = require('chalk');
+const no_pack_dir = ['node_modules', 'FaaS', 'sdk.js', 'locales', 'css', 'backend', 'sql', 'fonts', '.html', '.png', '.jpg', 'require.js', 'localforage.js', 'dataurl2blob.js', 'vconsole.js', 'vue.js', 'image-conversion.js'];
+
+((async () => {
+
+    const pack = async (source_path, target_path) => {
+        const match_dir = (path) => {
+            let isMatch = false;
+            no_pack_dir.forEach((value) => {
+                if (path.includes(value)) {
+                    isMatch = true;
+                    return;
+                };
+            });
+            return isMatch;
+        };
+
+        const pack_dir = async (source_path, target_path, isPack = true) => {
+
+            const isNoPack = match_dir(source_path);
+
+            if (isNoPack && isPack) return pack_dir(source_path, target_path, false);
+
+            const isDir = (await fs.stat(source_path)).isDirectory();
+
+            if (isDir) {
+                try {
+                    await fs.mkdir(target_path);
+                } catch (e) { };
+                isPack && console.time(chalk.green('Packing directory', source_path));
+                !isPack && console.time(chalk.yellow('Copying directory', target_path));
+                const dirFiles = await fs.readdir(source_path);
+                for (var i = 0, len = dirFiles.length; i < len; i++) {
+                    const file = dirFiles[i];
+                    await pack_dir(
+                        path.join(source_path, '/', file),
+                        path.join(target_path, '/', file),
+                        !isNoPack);
+                };
+                isPack && console.timeEnd(chalk.green('Packing directory', source_path));
+                !isPack && console.timeEnd(chalk.yellow('Copying directory', target_path));
+            } else {
+                pack_file(source_path, target_path, isPack);
+            };
+
+        };
+
+        const pack_file = async (source_path, target_path, isPack) => {
+            isPack && console.time(chalk.green('Packing file', source_path));
+            !isPack && console.time(chalk.green('Copying file', source_path));
+
+            const fileContent = (await fs.readFile(source_path, 'binary')).toString();
+
+            if (isPack) {
+                //paste to tmp directory
+                await fs.writeFile('./tmp/src/index.js', '/* For debug: location: ' + source_path + ' */' + fileContent, 'binary');
+                const target = (fileContent.includes('module.exports')) ? (
+                    'node'
+                ) : (
+                    'web'
+                );
+                try {
+                    await webpackPromise({
+                        entry: './tmp/src/index.js',
+                        output: {
+                            path: path.join(__dirname, 'tmp/dist'),
+                            filename: 'main.js',
+                            libraryTarget: 'umd'
+                        },
+                        mode: 'production',
+                        target: [target, 'es5'],
+                        module: {
+                            rules: [
+                                {
+                                    test: /(\.html|\.js)$/,
+                                    use: {
+                                        loader: 'babel-loader',
+                                        options: {
+                                            presets: [
+                                                [
+                                                    '@babel/preset-env', {
+                                                        // useBuiltIns: 'usage'
+                                                    },
+                                                ],
+
+                                            ],
+                                        },
+                                    },
+                                },
+                            ],
+                        },
+                    });
+                } catch (e) {
+                    console.error(chalk.red('Webpack throw an error\nFile:' + source_path));
+                    process.exit(1);
+                };
+                const packFileContent = await fs.readFile('./tmp/dist/main.js', 'binary');
+                await fs.writeFile(target_path, '/* Webpack & Concatenate browser pack tool\n * Source Path:' + source_path + '\n * Target: ' + target + ' */\n' + packFileContent, 'binary');
+            } else {
+                if (target_path.includes('.js')) await fs.writeFile(target_path, '/* Concatenate browser pack tool: This file is not compressed and converted into es5 */\n' + fileContent, 'binary');
+                else if (target_path.includes('.html')) await fs.writeFile(target_path, '<!-- Concatenate browser pack tool: This file is not compressed and converted into es5 -->\n' + fileContent, 'binary');
+                else await fs.writeFile(target_path, fileContent, 'binary');
+            };
+
+            isPack && console.timeEnd(chalk.green('Packing file', source_path));
+            !isPack && console.timeEnd(chalk.green('Copying file', source_path));
+        };
+
+        pack_dir(source_path, target_path);
+    };
+
+    try {
+        await fs.mkdir('./resource-web');
+    } catch (e) { };
+
+    try {
+        await fs.mkdir('./tmp');
+        await fs.mkdir('./tmp/src');
+        await fs.mkdir('./tmp/dist');
+    } catch (e) { };
+
+    await pack('./resource-app', './resource-web');
+
+    try {
+        await fs.rmdir('./tmp');
+    } catch (e) { };
+})());
