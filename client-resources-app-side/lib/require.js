19dd38fe7393bf76c6dd879cef0c080704b416eb
lihugang <lihugang@outlook.com>
2022-08-21 13:15:38 +0800

    Client: Resources for app side: require.js add virtual node environment to trick webpack

--- client-resources-app-side/lib/require.js
+++ client-resources-app-side/lib/require.js
@@ -1,24 +1,11 @@
-/**
- * @Author          : lihugang
- * @Date            : 2022-07-22 13:54:21
- * @LastEditTime    : 2022-07-30 17:45:02
- * @LastEditors     : lihugang
- * @Description     : 
- * @FilePath        : c:\Users\heche\AppData\Roaming\concatenate.pz6w7nkeote\resources\lib\require.js
- * @Copyright (c) lihugang
- * @长风破浪会有时 直挂云帆济沧海
- * @There will be times when the wind and waves break, and the sails will be hung straight to the sea.
- * @ * * * 
- * @是非成败转头空 青山依旧在 几度夕阳红
- * @Whether it's right or wrong, success or failure, it's all empty now, and it's all gone with the passage of time. The green hills of the year still exist, and the sun still rises and sets.
- */
+
 const _requireCache = new Map();
 const fRequire = function _require(module) {
     if (_requireCache.has(module)) return _requireCache.get(module);
     if (window.XMLHttpRequest) var xhr = new XMLHttpRequest();
     else var xhr = new ActiveXObject('Microsoft.XMLHTTP');
     xhr.open('GET', module, false); //get source code sync
-    xhr.onerror = function() { throw new Error('Cannot find module ' + module); };
+    xhr.onerror = function () { throw new Error('Cannot find module ' + module); };
     try {
         xhr.send();
     } catch (e) { throw new Error('Cannot find module ' + module); };
@@ -28,10 +15,13 @@ const fRequire = function _require(module) {
     };
     if (!module || !module.exports) {
         window.module = {
-            exports: null
+            exports: {}
         };
         window.exports = window.module.exports;
         //define module & exports
+
+        window.global = window;
+        //virtual node env
     };
     let evalFunc = new Function(xhr.responseText);
     evalFunc(); //run and parse
@@ -45,13 +35,14 @@ const fRequireAsync = async function _requireAsync(module) {
         if (_requireCache.has(module)) return resolve(_requireCache.get(module));
         const response = await fetch(module);
         if (!response.ok) throw new Error('Cannot find module ' + module);
-        var code = await(response).text();
+        var code = await (response).text();
         if (!module || !module.exports) {
             window.module = {
                 exports: null
             };
             window.exports = window.module.exports;
             //define module & exports
+            window.global = window;
         };
         let evalFunc = new Function(code);
         evalFunc(); //run and parse
